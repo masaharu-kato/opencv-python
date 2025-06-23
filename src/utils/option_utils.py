2b7f7ebccb4c2a38385c25aa83c99bd0e5225d35
Masaharu Kato <fudai.mk@gmail.com>
2025-06-23 23:45:37 +0900

    implement option loader

---
+++ src/utils/option_utils.py
@@ -0,0 +1,87 @@
+import logging
+from collections.abc import Callable, Iterable
+from dataclasses import MISSING, asdict, fields, _MISSING_TYPE
+from pathlib import Path
+from typing import Any, Literal
+
+
+def _construct_literal(value: Any, choices: list) -> Any:
+    # Handle Literal types by checking if the value is in the allowed options
+    if isinstance(value, str) and value in choices: # type: ignore
+        return value
+    else:
+        raise ValueError(f"Value '{value}' is not a valid option for {choices}")
+    
+def _construct_list(value: Any, elm_constructor: Callable) -> list:
+    # Handle list types by converting the value to a list if it is not already
+    if isinstance(value, Iterable) and not isinstance(value, str):
+        return list(value)
+    elif isinstance(value, str):
+        return list(map(elm_constructor, value.split(',')))  # Split by comma if it's a string
+    else:
+        raise ValueError(f"Value '{value}' cannot be converted to a list")
+    
+    
+def get_constructor_from_type_hint(type_hint: Any) -> Callable:
+    """
+    Get the constructor and default value for a given type hint.
+    """
+    if type_hint is bool:
+        return bool
+    elif type_hint is int:
+        return int
+    elif type_hint is float:
+        return float
+    elif type_hint is str:
+        return str
+    elif type_hint is Path:
+        return Path
+    elif hasattr(type_hint, '__origin__') and type_hint.__origin__ is Literal:
+        # Handle Literal types
+        return lambda v:_construct_literal(v, type_hint.__args__)
+    elif hasattr(type_hint, '__origin__') and type_hint.__origin__ is list:
+        return lambda v: _construct_list(v, get_constructor_from_type_hint(type_hint.__args__[0]))
+    elif hasattr(type_hint, '__origin__') and type_hint.__origin__ is dict:
+        return dict
+    elif hasattr(type_hint, '__origin__') and type_hint.__origin__ is tuple:
+        return tuple
+    else:
+        raise ValueError(f"Unsupported type hint: {type_hint}")
+
+
+
+def make_options(cls, cp, args):
+    """
+    Convert a checkpoint and arguments into a dataclass instance.
+    """
+    init_args = {}
+    error_fields = set()
+
+    cpvals = {} if cp is None else cp if isinstance(cp, dict) else asdict(cp)
+    
+    logging.info(f"  {cls.__name__}")
+    # Convert types if necessary
+    for field in fields(cls):
+
+        field_name = field.name
+        constructor = get_constructor_from_type_hint(field.type)
+        default_value = MISSING if isinstance(field.default, _MISSING_TYPE) else field.default
+
+        cp_value = cpvals.get(field_name)
+        arg_value = args.get(field_name)
+        if arg_value is not None:
+            init_args[field_name] = constructor(arg_value)
+            logging.info(f"    {field_name}: {init_args[field_name]} (from args)")
+        elif cp_value is not None:
+            init_args[field_name] = constructor(cp_value)
+            logging.info(f"    {field_name}: {init_args[field_name]} (from cp)")
+        elif default_value is not MISSING:
+            init_args[field_name] = default_value
+            logging.info(f"    {field_name}: {init_args[field_name]} (from defaults)")
+        else:
+            error_fields.add(field_name)
+
+    if error_fields:
+        raise ValueError(f"Missing required fields in {cls.__name__}: {', '.join(error_fields)}")
+    
+    return cls(**init_args)
